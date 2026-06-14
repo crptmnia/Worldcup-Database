28c55ae9aa3afb7c8ffd1e36e10554cb587c8c9b
crptmnia <jgperillo@up.edu.ph>
2026-06-14 00:03:49 -0400

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,35 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# INSERTING IN TEAMS TABLE
+# For testing
+$PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY CASCADE"
+
+# Fetch unique team names
+awk -F',' 'NR>1 {print $3"\n"$4}' games.csv | sort | uniq | while read TEAM
+# Insert to database
+do
+  echo "Inserting $TEAM..."
+  TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$TEAM'")
+
+  if [[ -z $TEAM_ID ]]
+  then
+    $PSQL "INSERT INTO teams(name) VALUES('$TEAM')"
+  fi
+done
+echo "Inserted succesfully."
+
+# INSERTING INTO GAMES TABLE
+# Fetch info
+awk -F',' 'NR>1 {print $1","$2","$3","$4","$5","$6}' games.csv |
+while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOAL OPPONENT_GOAL
+do
+  # Query the team names from teams table first
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'") 
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  
+  # Insert the values
+  echo "Inserting..."
+  $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOAL, $OPPONENT_GOAL)"
+done
